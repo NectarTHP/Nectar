9d49bb03c6f4d5a96fac0eb62e1521aae7dc029f
jayrom3 <jerome@lena-fr.com>
2019-09-13 22:37:52 +0200

    [FIX] DELETE email sender

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,8 +48,6 @@ class UsersController < ApplicationController
     # Create the user from params
     @user = User.new(user_params)
     if @user.save
-      # Deliver the signup email
-      # UserNotifierMailer.send_signup_email(@user).deliver
       redirect_to(@user, :notice => 'User created')
     else
       render :action => 'new'
