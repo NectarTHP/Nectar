38bd88a5d530c0c0145cd8286dbeef1db49b0f09
Thibobo78 <thibault.collard@gmail.com>
2019-09-13 19:02:41 +0200

    [TEST] Db user with details update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ def configure_devise_parameters
 
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :comment, :is_admin, :is_artist, :username, :first_name, :last_name, :bio, :dateofbirth, :siret, :adress, :zipcode, :city, :country, :mobile, :fb_user_ID, :insta_user_ID)}
 
-  end
+end
+end
 
