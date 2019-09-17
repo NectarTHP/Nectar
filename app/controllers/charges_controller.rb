b955e3fe8d4d31a08250e86d3d9a0c59ed9d5a61
jayrom3 <jerome@lena-fr.com>
2019-09-17 12:32:35 +0200

    [UPD] Order workflow is working (cart, checkout, stripe, create order, empty cart)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,12 +1,18 @@
 class ChargesController < ApplicationController
 
   def new
+    puts "$"*60;puts "beginning of charges_controller#new";puts "$"*60
+    @cart = Cart.find_by(user_id: current_user)
+    @order = Order.new
+    puts "$"*60;puts "end of charges_controller#new";puts "$"*60
   end
   
   def create
-    # Amount in cents
-    @amount = 500
   
+    puts "$"*60;puts "beginning of charges_controller#create";puts "$"*60
+
+    @cart = Cart.find_by(user_id: current_user)
+
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
       source: params[:stripeToken],
@@ -14,14 +20,27 @@ class ChargesController < ApplicationController
   
     charge = Stripe::Charge.create({
       customer: customer.id,
-      amount: @amount,
+      amount: @cart.cart_total,
       description: 'Rails Stripe customer',
-      currency: 'usd',
+      currency: 'eur',
     })
-  
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
-  
+
+    binding.pry #3
+
+    @order = Order.new(user_id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email)
+    @order.save
+    @cart.line_items.each do |item|
+      item.update(order_id: @order.id)
+      item.update(cart_id: nil)
+      puts item
+    end
+    
+    binding.pry #4
+
+    puts "$"*60;puts "Create order and empty cart code executed";puts "$"*60
+
+  end #create
+
+  puts "$"*60;puts "end of charges_controller#create";puts "$"*60
+
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @orders = Order.all
   end
@@ -9,18 +11,43 @@ class OrdersController < ApplicationController
 
   def new
     @cart = Cart.find_by(user_id: current_user)
-
     @order = Order.new
   end
 
   def create
-    @order = Order.new(order_params)
+
+    @cart = Cart.find_by(user_id: current_user)
+    
+    puts "$"*60;puts "beginning of strip code in orders#create";puts "$"*60
+
+    @amount = 500
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @cart.cart_total,
+      description: 'Rails Stripe customer',
+      currency: 'eur',
+    })
+  
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    
+  puts "$"*60;puts "end of strip code in orders#create";puts "$"*60
+
+    @order = Order.new(user_id: current_user.id, first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email)
     @cart.line_items.each do |item|
-      @order.line_items << item
+      item.order_id = @order.id
       item.cart_id = nil
     end
     @order.save
     redirect_to root_path
+
   end
   
   private
