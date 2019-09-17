4854e657d4d3bd5040f33993173bdba9508f944b
jayrom3 <jerome@lena-fr.com>
2019-09-17 16:29:11 +0200

    [FIX] Fixing deleted migration (part 2 of 2)

--- db/schema.rb
+++ db/schema.rb
@@ -114,14 +114,6 @@ ActiveRecord::Schema.define(version: 2019_09_16_093003) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
-    t.integer "item_qty"
-    t.integer "order_number"
-    t.string "tracking_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
